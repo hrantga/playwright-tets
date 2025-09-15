1833492b32d200bc9aa8e4c808ae20565ea6b421
hrant <hrantgrigoryan9@gmail.com>
2025-09-15 18:29:11 +0400

    change API requestes to mock data

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,14 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com'
+import { mockUsers, mockPosts } from './mockData.js'
 
 export async function get(path) {
+  if (path === '/users') {
+    return Promise.resolve(mockUsers)
+  }
+  if (path === '/posts') {
+    return Promise.resolve(mockPosts)
+  }
+
   const url = `${BASE_URL}${path}`
   const res = await fetch(url)
   if (!res.ok) {
