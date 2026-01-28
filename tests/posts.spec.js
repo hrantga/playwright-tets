930200154fe38fb5dec79ce0cbe70f272ea03377
Mantun Kumar <mantun20singh@gmail.com>
2026-01-28 14:54:39 +0530

    updated tests

--- tests/posts.spec.js
+++ tests/posts.spec.js
@@ -1,5 +1,29 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
 
 test('filter posts, verify, and export CSV', async ({ page, context }) => {
+
+    // validate login successful
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('demo', 'pass123');
+    await expect(page).toHaveTitle('Vite + React')
+
+    // navigate to posts page and validate filtering
+    await loginPage.goToPosts();
+    await loginPage.filterPosts('sunt aut facere repellat');
+
+    // export CSV and validate download
+    const [ download ] = await Promise.all([
+        context.waitForEvent('download'),
+        loginPage.exportCSV()
+    ]);
+    const path = await download.path();
+    expect(path).not.toBeNull();
+
+    // view a post and validate view pop up
+    await loginPage.viewPost(); 
+    await loginPage.closeDrawer();
   
 })
--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,6 +1,16 @@
-import { test, expect } from '@playwright/test'
-
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
 test('users list can sort A→Z', async ({ page }) => {
+
+    // validate login successful
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('demo', 'pass123');
+    await expect(page).toHaveTitle('Vite + React')
+
+    // navigate to users page and validate sorting
+    await loginPage.goToUsers();
+    await loginPage.sortUsers();
  
 })
